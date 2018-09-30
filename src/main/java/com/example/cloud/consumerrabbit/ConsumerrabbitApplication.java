f702a41352797614bb78de8a70f0b6864379d7d2
saurabh <saurabh945644@gmail.com>
2018-09-30 18:58:55 +0530

    First Commit

---
+++ src/main/java/com/example/cloud/consumerrabbit/ConsumerrabbitApplication.java
@@ -0,0 +1,23 @@
+package com.example.cloud.consumerrabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+
+@EnableBinding(Sink.class)
+@SpringBootApplication
+public class ConsumerrabbitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerrabbitApplication.class, args);
+    }
+
+    @StreamListener(target=Sink.INPUT)
+    public void logfast(String msg) {
+        System.out.println(msg);
+    }
+}
+
+
